bf237395f35d2e71d74db170995a6351cc03bc55
Lukas Weber <lweber@physik.rwth-aachen.de>
2019-06-26 10:58:26 +0200

    more sensible error when adding to a non-existing observable

--- src/measurements.h
+++ src/measurements.h
@@ -32,6 +32,9 @@ private:
 
 template<class T>
 void measurements::add(const std::string name, T value) {
+	if(observables_.count(name) == 0) {
+		throw std::runtime_error{fmt::format("tried to add to observable '{}' which was not registered!", name)};
+	}
 	observables_.at(name).add(value);
 }
 }
